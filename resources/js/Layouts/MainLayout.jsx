9fbad11ae79c01824fc682dc553cc689020828de
misawa-shota <kaiyou0120@gmail.com>
2025-10-21 00:23:11 +0900

    ドロワーメニューの作成 #5

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -1,15 +1,61 @@
-import { Box, Heading, HStack, Text } from '@chakra-ui/react';
+import { Box, Drawer, Heading, HStack, Icon, Link, Portal, Text, VStack } from '@chakra-ui/react';
 import React from 'react';
+import { IoMenu, IoClose } from "react-icons/io5";
+import { usePage } from '@inertiajs/react';
 
 const MainLayout = ({children}) => {
+    const { auth } = usePage().props;
+
     return (
         <>
             <header>
-                <Box bg={"yellow.400"} p={3}>
-                    <HStack>
-                        <Heading fontSize={"3xl"}>{import.meta.env.VITE_APP_NAME}</Heading>
-                    </HStack>
-                </Box>
+                {/* Drawer */}
+                <Drawer.Root>
+                    {/* header */}
+                    <Box bg={"yellow.400"} p={3}>
+                        <HStack display={"flex"} alignItems={"center"}justifyContent={"space-between"}>
+                            <Heading as={"h1"} fontSize={"3xl"}>
+                                <Link href={route('shop.index')}>
+                                    {import.meta.env.VITE_APP_NAME}
+                                </Link>
+                            </Heading>
+                            <Drawer.Trigger asChild>
+                                <Icon as={IoMenu} fontSize={"30px"}></Icon>
+                            </Drawer.Trigger>
+                        </HStack>
+                    </Box>
+                    {/* header */}
+                    <Portal>
+                        <Drawer.Backdrop/>
+                        <Drawer.Positioner>
+                            <Drawer.Content>
+                                <Drawer.Header>
+                                    <Drawer.Title>menu</Drawer.Title>
+                                </Drawer.Header>
+                                <Drawer.Body>
+                                    <VStack>
+                                        {auth.user ? (
+                                            <>
+                                                <Text>こんにちは{auth.user.name}さん</Text>
+                                                <Link href={route('dashboard')}>マイページ</Link>
+                                                <Link>レビュー投稿</Link>
+                                                <Link href={route('logout')}>ログアウト</Link>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <Link href={route('login')}>ログイン</Link>
+                                                <Link href={route('register')}>新規登録</Link>
+                                            </>
+                                        )}
+                                    </VStack>
+                                </Drawer.Body>
+                                <Drawer.CloseTrigger asChild>
+                                    <Icon as={IoClose} />
+                                </Drawer.CloseTrigger>
+                            </Drawer.Content>
+                        </Drawer.Positioner>
+                    </Portal>
+                </Drawer.Root>
             </header>
             <div>{children}</div>
             <footer>
--- resources/js/Pages/Shop/Detail.jsx
+++ resources/js/Pages/Shop/Detail.jsx
@@ -22,21 +22,25 @@ const Detail = (props) => {
             </HStack>
             <Heading as={"h3"} fontSize={"20px"} fontWeight={"bold"} mt={10}>レビュー一覧</Heading>
             <Box py={3} spaceY={5}>
-                {props.shop.reviews.map((review) => (
-                    <Box key={review.id} spaceY={3} p={3} borderRadius={"md"} borderWidth={"1px"} borderColor={"gray.500"}>
-                        <HStack display={"flex"} alignItems={"center"}>
-                            {Array(5).fill("").map((_, i) => (
-                                review.rate > 0 ? (
-                                    <Icon key={i} as={FaStar} size={"md"} color={i < review.rate ? "yellow.500" : "gray.500"} />
-                                ) : (
-                                    <Icon key={i} as={FaStar} size={"lg"} color={"gray.500"} />
-                                )
-                            ))}
-                            <Text ml={5}>{review.user.name}さん</Text>
-                        </HStack>
-                        <Text>{review.comment}</Text>
-                    </Box>
-                ))}
+                {props.shop.reviews.length > 0 ? (
+                    props.shop.reviews.map((review) => (
+                        <Box key={review.id} spaceY={3} p={3} borderRadius={"md"} borderWidth={"1px"} borderColor={"gray.500"}>
+                            <HStack display={"flex"} alignItems={"center"}>
+                                {Array(5).fill("").map((_, i) => (
+                                    review.rate > 0 ? (
+                                        <Icon key={i} as={FaStar} size={"md"} color={i < review.rate ? "yellow.500" : "gray.500"} />
+                                    ) : (
+                                        <Icon key={i} as={FaStar} size={"lg"} color={"gray.500"} />
+                                    )
+                                ))}
+                                <Text ml={5}>{review.user.name}さん</Text>
+                            </HStack>
+                            <Text>{review.comment}</Text>
+                        </Box>
+                    ))
+                ) : (
+                    <Text>レビューはまだありません</Text>
+                )}
             </Box>
         </Box>
     )
