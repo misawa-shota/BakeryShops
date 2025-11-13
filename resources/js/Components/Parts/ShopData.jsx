466eace9e766e19a24aa0b7b080a74f65c280eff
misawa-shota <kaiyou0120@gmail.com>
2025-11-14 00:18:57 +0900

    各パーツのカスタムコンポーネント化#21

---
+++ resources/js/Components/Parts/ShopData.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import HomeStarRating from './HomeStarRating';
+import { HStack, Image, VStack, Heading, Box, Text } from '@chakra-ui/react';
+
+function ShopData(props) {
+    return (
+        <>
+            {props.homeShopData === "トップページの店舗情報" && (
+                <HStack>
+                    <Image src={"https://placehold.jp/150x150.png"} />
+                    <VStack ml={5} alignItems={"flex-start"}>
+                        <Heading fontSize={"20px"} fontWeight={"bold"}>{props.shopName}</Heading>
+                        <Box display={"flex"} arignItems={"center"}>
+                            <HomeStarRating rating={props.shopReviewsAvgRate} percentage={parseFloat(Number(props.shopReviewsAvgRate).toFixed(1).slice(2)) * 10} />
+                            <Text ml={5}>レビュー合計: {props.shopReviewsCount}件</Text>
+                        </Box>
+                    </VStack>
+                </HStack>
+            )}
+            {props.detailShopData === "詳細ページの店舗情報" && (
+                <>
+                    <Heading my={5} as={"h2"} fontSize={"30px"} fontWeight={"bold"}>{props.shopName}</Heading>
+                    <HStack display={"flex"} alignItems={"center"} spaceX={5}>
+                        <Image src={"https://placehold.jp/150x150.png"} width={"300px"} />
+                        <Box spaceY={5}>
+                            <Box>
+                                <Text fontWeight={"bold"}>所在地</Text>
+                                <Text>{props.shopLocate}</Text>
+                            </Box>
+                            <Box>
+                                <Text fontWeight={"bold"}>店舗情報</Text>
+                                <Text>{props.shopDescription}</Text>
+                            </Box>
+                        </Box>
+                    </HStack>
+                </>
+            )}
+        </>
+    )
+}
+
+export default ShopData
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,53 +1,26 @@
 import React from 'react';
 import MainLayout from '@/Layouts/MainLayout';
-import { Heading, Box, Link, VStack, HStack, Image, Text, Button } from '@chakra-ui/react';
-import StarRating from '@/Components/Custom/StarRating';
-import { router } from '@inertiajs/react';
+import { Heading, Box, VStack } from '@chakra-ui/react';
+import CardList from '@/Components/Molecules/CardList';
+import Pagination from '@/Components/Parts/Pagination';
 
 const Home = (props) => {
-    const handlePageChange = (url) => {
-        router.get(url);
-    };
-
-    const getButtonLabel = (label) => {
-        if(label.includes("previous")) return "前へ";
-        if(label.includes("next")) return "次へ";
-        return label;
-    };
 
     return (
         <Box>
             <Heading fontSize={{base: "24px"}} mb={10} fontWeight={"bold"}>店舗一覧</Heading>
             <Box>
                 <VStack spaceY={5} align={"stretch"}>
-                {props.shops.data.map((shop) => (
-                    <Link href={route('shop.detail', {id: shop.id})} key={shop.id} borderWidth={"1px"} borderRadius={"md"}>
-                        <HStack>
-                            <Image src={"https://placehold.jp/150x150.png"} />
-                            <VStack ml={5} alignItems={"flex-start"}>
-                                <Heading fontSize={"20px"} fontWeight={"bold"}>{shop.name}</Heading>
-                                <Box display={"flex"} arignItems={"center"}>
-                                    <StarRating rating={shop.reviews_avg_rate} percentage={parseFloat(Number(shop.reviews_avg_rate).toFixed(1).slice(2)) * 10} />
-                                    <Text ml={5}>レビュー合計: {shop.reviews_count}件</Text>
-                                </Box>
-                            </VStack>
-                        </HStack>
-                    </Link>
-                ))}
+                    <CardList
+                        cardList={"店舗リスト"}
+                        shopLists={props.shops.data}
+                    />
                 </VStack>
             </Box>
-            <HStack justifyContent={"center"} alignItems={"center"} mt={5}>
-                {props.shops.links.map((link, index) => (
-                    <Button
-                        key={index}
-                        onClick={() => handlePageChange(link.url)}
-                        bg={link.active ? "yellow.500" : "gray.100"}
-                        isDisabled={!link.url}
-                    >
-                        {getButtonLabel(link.label)}
-                    </Button>
-                ))}
-            </HStack>
+
+            {/* ページネーション */}
+            <Pagination shopsLinks={props.shops.links} />
+            {/* ページネーション */}
         </Box>
     );
 };
